9b43cae513b9cfb5a27e1c21a1f1fc2fed9d184e
Ian Arch <ian.arch01@gmail.com>
2021-05-20 09:29:33 -0600

    Major progress *salutes*

--- PokeFlask.py
+++ PokeFlask.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify,render_template
 
 
 #################################################
@@ -26,26 +26,53 @@ app = Flask(__name__)
 #################################################
 
 @app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/api")
 def welcome():
     """List all available api routes."""
     return (
         f"Available Routes:<br/>"
-        f"/api/v1.0/pokemon"
+        f"/api/v1.0/types<br/>"
+        f"/api/v1.0/dragon<br/>"
+        f"/api/v1.0/ghost<br/>"
+        f"/api/v1.0/normal<br/>"
+        f"/api/v1.0/poison<br/>"
+        f"/api/v1.0/grass<br/>"
+        f"/api/v1.0/psychic<br/>"
+        f"/api/v1.0/fighting<br/>"
+        f"/api/v1.0/electric<br/>"
+        f"/api/v1.0/ice<br/>"
+        f"/api/v1.0/fire<br/>"
+        f"/api/v1.0/bug<br/>"
+        f"/api/v1.0/ground<br/>"
+        f"/api/v1.0/rock<br/>"
+        f"/api/v1.0/water<br/>"
+        f"/api/v1.0/fairy<br/>"
+
     )
 
 
-@app.route("/api/v1.0/pokemon")
-def pokemon():
-    # Create our session (link) from Python to the DB
-    # session = Session(engine)
+@app.route("/api/v1.0/types")
+def types():
+    
+    results = engine.execute('SELECT DISTINCT "pokeType" FROM gen1_data')
 
-    # Query all pokemon
-    # results = session.query(Pokemon.name, Pokemon.pokeType, Pokemon.hp, Pokemon.attack, Pokemon.defense, Pokemon.specialAttack, Pokemon.specialDefense, Pokemon.speed).all()
+    all_pokemon = []
+    for pokeType in results:
+        print(pokeType)
+        pokemon_dict = {}
+        pokemon_dict["pokeType"] = pokeType[0]
+        all_pokemon.append(pokemon_dict)
 
-    # session.close()
-    results = engine.execute("SELECT * FROM gen1_data")
+    return jsonify(all_pokemon)
 
-    # Create a dictionary from the row data and append to a list of all_pokemon
+@app.route("/api/v1.0/dragon")
+def dragon():
+    
+    results = engine.execute('SELECT * FROM gen1_data WHERE "pokeType" = \'dragon\'')
+
     all_pokemon = []
     for name, pokeType, hp, attack, defense, specialAttack, specialDefense, speed in results:
         pokemon_dict = {}
@@ -61,8 +88,285 @@ def pokemon():
 
     return jsonify(all_pokemon)
 
+@app.route("/api/v1.0/ghost")
+def ghost():
+    
+    results = engine.execute('SELECT * from gen1_data WHERE "pokeType" = \'ghost\'')
 
+    all_pokemon = []
+    for name, pokeType, hp, attack, defense, specialAttack, specialDefense, speed in results:
+        pokemon_dict = {}
+        pokemon_dict["name"] = name
+        pokemon_dict["pokeType"] = pokeType
+        pokemon_dict["hp"] = hp
+        pokemon_dict["attack"] = attack
+        pokemon_dict["defense"] = defense
+        pokemon_dict["specialAttack"] = specialAttack
+        pokemon_dict["specialDefense"] = specialDefense
+        pokemon_dict["speed"] = speed
+        all_pokemon.append(pokemon_dict)
 
+    return jsonify(all_pokemon)
+
+@app.route("/api/v1.0/normal")
+def normal():
+    
+    results = engine.execute('SELECT * from gen1_data WHERE "pokeType" = \'normal\'')
+
+    all_pokemon = []
+    for name, pokeType, hp, attack, defense, specialAttack, specialDefense, speed in results:
+        pokemon_dict = {}
+        pokemon_dict["name"] = name
+        pokemon_dict["pokeType"] = pokeType
+        pokemon_dict["hp"] = hp
+        pokemon_dict["attack"] = attack
+        pokemon_dict["defense"] = defense
+        pokemon_dict["specialAttack"] = specialAttack
+        pokemon_dict["specialDefense"] = specialDefense
+        pokemon_dict["speed"] = speed
+        all_pokemon.append(pokemon_dict)
+
+    return jsonify(all_pokemon)
+
+@app.route("/api/v1.0/poison")
+def poison():
+    
+    results = engine.execute('SELECT * from gen1_data WHERE "pokeType" = \'poison\'')
+
+    all_pokemon = []
+    for name, pokeType, hp, attack, defense, specialAttack, specialDefense, speed in results:
+        pokemon_dict = {}
+        pokemon_dict["name"] = name
+        pokemon_dict["pokeType"] = pokeType
+        pokemon_dict["hp"] = hp
+        pokemon_dict["attack"] = attack
+        pokemon_dict["defense"] = defense
+        pokemon_dict["specialAttack"] = specialAttack
+        pokemon_dict["specialDefense"] = specialDefense
+        pokemon_dict["speed"] = speed
+        all_pokemon.append(pokemon_dict)
+
+    return jsonify(all_pokemon)
+
+@app.route("/api/v1.0/grass")
+def grass():
+    
+    results = engine.execute('SELECT * from gen1_data WHERE "pokeType" = \'grass\'')
+
+    all_pokemon = []
+    for name, pokeType, hp, attack, defense, specialAttack, specialDefense, speed in results:
+        pokemon_dict = {}
+        pokemon_dict["name"] = name
+        pokemon_dict["pokeType"] = pokeType
+        pokemon_dict["hp"] = hp
+        pokemon_dict["attack"] = attack
+        pokemon_dict["defense"] = defense
+        pokemon_dict["specialAttack"] = specialAttack
+        pokemon_dict["specialDefense"] = specialDefense
+        pokemon_dict["speed"] = speed
+        all_pokemon.append(pokemon_dict)
+
+    return jsonify(all_pokemon)
+
+@app.route("/api/v1.0/psychic")
+def psychic():
+    
+    results = engine.execute('SELECT * from gen1_data WHERE "pokeType" = \'psychic\'')
+
+    all_pokemon = []
+    for name, pokeType, hp, attack, defense, specialAttack, specialDefense, speed in results:
+        pokemon_dict = {}
+        pokemon_dict["name"] = name
+        pokemon_dict["pokeType"] = pokeType
+        pokemon_dict["hp"] = hp
+        pokemon_dict["attack"] = attack
+        pokemon_dict["defense"] = defense
+        pokemon_dict["specialAttack"] = specialAttack
+        pokemon_dict["specialDefense"] = specialDefense
+        pokemon_dict["speed"] = speed
+        all_pokemon.append(pokemon_dict)
+
+    return jsonify(all_pokemon)
+
+@app.route("/api/v1.0/fighting")
+def fighting():
+    
+    results = engine.execute('SELECT * from gen1_data WHERE "pokeType" = \'fighting\'')
+
+    all_pokemon = []
+    for name, pokeType, hp, attack, defense, specialAttack, specialDefense, speed in results:
+        pokemon_dict = {}
+        pokemon_dict["name"] = name
+        pokemon_dict["pokeType"] = pokeType
+        pokemon_dict["hp"] = hp
+        pokemon_dict["attack"] = attack
+        pokemon_dict["defense"] = defense
+        pokemon_dict["specialAttack"] = specialAttack
+        pokemon_dict["specialDefense"] = specialDefense
+        pokemon_dict["speed"] = speed
+        all_pokemon.append(pokemon_dict)
+
+    return jsonify(all_pokemon)
+
+@app.route("/api/v1.0/electric")
+def electric():
+    
+    results = engine.execute('SELECT * from gen1_data WHERE "pokeType" = \'electric\'')
+
+    all_pokemon = []
+    for name, pokeType, hp, attack, defense, specialAttack, specialDefense, speed in results:
+        pokemon_dict = {}
+        pokemon_dict["name"] = name
+        pokemon_dict["pokeType"] = pokeType
+        pokemon_dict["hp"] = hp
+        pokemon_dict["attack"] = attack
+        pokemon_dict["defense"] = defense
+        pokemon_dict["specialAttack"] = specialAttack
+        pokemon_dict["specialDefense"] = specialDefense
+        pokemon_dict["speed"] = speed
+        all_pokemon.append(pokemon_dict)
+
+    return jsonify(all_pokemon)
+
+@app.route("/api/v1.0/ice")
+def ice():
+    
+    results = engine.execute('SELECT * from gen1_data WHERE "pokeType" = \'ice\'')
+
+    all_pokemon = []
+    for name, pokeType, hp, attack, defense, specialAttack, specialDefense, speed in results:
+        pokemon_dict = {}
+        pokemon_dict["name"] = name
+        pokemon_dict["pokeType"] = pokeType
+        pokemon_dict["hp"] = hp
+        pokemon_dict["attack"] = attack
+        pokemon_dict["defense"] = defense
+        pokemon_dict["specialAttack"] = specialAttack
+        pokemon_dict["specialDefense"] = specialDefense
+        pokemon_dict["speed"] = speed
+        all_pokemon.append(pokemon_dict)
+
+    return jsonify(all_pokemon)
+
+@app.route("/api/v1.0/fire")
+def fire():
+    
+    results = engine.execute('SELECT * from gen1_data WHERE "pokeType" = \'fire\'')
+
+    all_pokemon = []
+    for name, pokeType, hp, attack, defense, specialAttack, specialDefense, speed in results:
+        pokemon_dict = {}
+        pokemon_dict["name"] = name
+        pokemon_dict["pokeType"] = pokeType
+        pokemon_dict["hp"] = hp
+        pokemon_dict["attack"] = attack
+        pokemon_dict["defense"] = defense
+        pokemon_dict["specialAttack"] = specialAttack
+        pokemon_dict["specialDefense"] = specialDefense
+        pokemon_dict["speed"] = speed
+        all_pokemon.append(pokemon_dict)
+
+    return jsonify(all_pokemon)
+
+@app.route("/api/v1.0/bug")
+def bug():
+    
+    results = engine.execute('SELECT * from gen1_data WHERE "pokeType" = \'bug\'')
+
+    all_pokemon = []
+    for name, pokeType, hp, attack, defense, specialAttack, specialDefense, speed in results:
+        pokemon_dict = {}
+        pokemon_dict["name"] = name
+        pokemon_dict["pokeType"] = pokeType
+        pokemon_dict["hp"] = hp
+        pokemon_dict["attack"] = attack
+        pokemon_dict["defense"] = defense
+        pokemon_dict["specialAttack"] = specialAttack
+        pokemon_dict["specialDefense"] = specialDefense
+        pokemon_dict["speed"] = speed
+        all_pokemon.append(pokemon_dict)
+
+    return jsonify(all_pokemon)
+
+@app.route("/api/v1.0/ground")
+def ground():
+    
+    results = engine.execute('SELECT * from gen1_data WHERE "pokeType" = \'ground\'')
+
+    all_pokemon = []
+    for name, pokeType, hp, attack, defense, specialAttack, specialDefense, speed in results:
+        pokemon_dict = {}
+        pokemon_dict["name"] = name
+        pokemon_dict["pokeType"] = pokeType
+        pokemon_dict["hp"] = hp
+        pokemon_dict["attack"] = attack
+        pokemon_dict["defense"] = defense
+        pokemon_dict["specialAttack"] = specialAttack
+        pokemon_dict["specialDefense"] = specialDefense
+        pokemon_dict["speed"] = speed
+        all_pokemon.append(pokemon_dict)
+
+    return jsonify(all_pokemon)
+
+@app.route("/api/v1.0/rock")
+def rock():
+    
+    results = engine.execute('SELECT * from gen1_data WHERE "pokeType" = \'rock\'')
+
+    all_pokemon = []
+    for name, pokeType, hp, attack, defense, specialAttack, specialDefense, speed in results:
+        pokemon_dict = {}
+        pokemon_dict["name"] = name
+        pokemon_dict["pokeType"] = pokeType
+        pokemon_dict["hp"] = hp
+        pokemon_dict["attack"] = attack
+        pokemon_dict["defense"] = defense
+        pokemon_dict["specialAttack"] = specialAttack
+        pokemon_dict["specialDefense"] = specialDefense
+        pokemon_dict["speed"] = speed
+        all_pokemon.append(pokemon_dict)
+
+    return jsonify(all_pokemon)
+
+@app.route("/api/v1.0/water")
+def water():
+    
+    results = engine.execute('SELECT * from gen1_data WHERE "pokeType" = \'water\'')
+
+    all_pokemon = []
+    for name, pokeType, hp, attack, defense, specialAttack, specialDefense, speed in results:
+        pokemon_dict = {}
+        pokemon_dict["name"] = name
+        pokemon_dict["pokeType"] = pokeType
+        pokemon_dict["hp"] = hp
+        pokemon_dict["attack"] = attack
+        pokemon_dict["defense"] = defense
+        pokemon_dict["specialAttack"] = specialAttack
+        pokemon_dict["specialDefense"] = specialDefense
+        pokemon_dict["speed"] = speed
+        all_pokemon.append(pokemon_dict)
+
+    return jsonify(all_pokemon)
+
+@app.route("/api/v1.0/fairy")
+def fairy():
+    
+    results = engine.execute('SELECT * from gen1_data WHERE "pokeType" = \'fairy\'')
+
+    all_pokemon = []
+    for name, pokeType, hp, attack, defense, specialAttack, specialDefense, speed in results:
+        pokemon_dict = {}
+        pokemon_dict["name"] = name
+        pokemon_dict["pokeType"] = pokeType
+        pokemon_dict["hp"] = hp
+        pokemon_dict["attack"] = attack
+        pokemon_dict["defense"] = defense
+        pokemon_dict["specialAttack"] = specialAttack
+        pokemon_dict["specialDefense"] = specialDefense
+        pokemon_dict["speed"] = speed
+        all_pokemon.append(pokemon_dict)
+
+    return jsonify(all_pokemon)
 
 
 if __name__ == '__main__':
---
+++ static/app.js
@@ -0,0 +1,113 @@
+// Use D3 fetch to read the JSON file
+// The data from the JSON file is arbitrarily named importedData as the argument
+
+function buildPlot(id) {
+    d3.json("/api/v1.0/" + id).then((importedData) => {
+    // console.log(importedData);
+        var data = importedData;
+
+        data.sort(function(a, b) {
+        return parseFloat(b.hp) - parseFloat(a.hp);
+        });
+    
+        var trace1 = {
+        y: data.map(row => row.hp),
+        x: data.map(row => row.name),
+        text: data.map(row => row.name),
+        name: "Health",
+        type: "bar",
+        orientation: "v"
+        };
+    
+        // data
+        var chartData = [trace1];
+    
+        // Apply the group bar mode to the layout
+        var layout = {
+        title: "helf",
+        margin: {
+            l: 100,
+            r: 100,
+            t: 100,
+            b: 100
+        },
+        height: 600,
+        width: 1200
+        };
+    
+        // Render the plot to the div tag with id "plot"
+        Plotly.newPlot("bar", chartData, layout);
+
+        var data2 = importedData;
+        data2.sort(function(a, b) {
+            return parseFloat(b.attack) - parseFloat(a.attack);
+        });
+        // data2 = data2.slice(0,5);
+        var trace2 = {
+            x: data2.map(row=>row.name),
+            y:data2.map(row=>row.attack),
+            text: data2.map(row=>row.name),
+            name: "Attack",
+            type: 'scatter',
+            mode: 'lines+markers'
+        };
+        var chartData2 = [trace2];
+        var layout2 = {
+            title: "attac",
+            margin: {
+                l: 100,
+                r: 100,
+                t: 100,
+                b: 100
+            },
+            height: 600,
+            width: 1200
+        };
+        Plotly.newPlot("scatter",chartData2,layout2);
+  });
+}
+
+// function plotInfo(id) {
+//     d3.json("flaskDataPull.json").then((data)=> {
+//         var metadata = data;
+//         var metadataFilter = metadata.filter(meta => meta.id.toString() === id)[0];
+
+//         var pokeSet = d3.select("#sample-metadata");
+//         pokeSet.html("");
+        
+//         Object.entries(metadataFilter).forEach((key)=> {
+//             pokeSet.append("h5").text(key[0].toUpperCase() + ": " + key[1] + "\n");
+//         });
+//     });
+// }
+
+// function sampleSelection(id) {
+//     buildPlot(id);
+//     plotInfo(id);
+// }
+
+function init() {
+    var dropdownMenu = d3.select("#selDataset");
+    d3.json("/api/v1.0/types").then((data)=>{
+        var pokeList = []
+        data.forEach(function(type) {
+            if (pokeList.indexOf(type.pokeType)<0) {
+                pokeList.push(type.pokeType)
+            }
+        });
+        //  loop through to find if type exists, append
+        pokeList.forEach(function(type) {
+            dropdownMenu.append("option").text(type).property("value");
+        });
+        // Binding event
+        dropdownMenu.on("change",function(){
+            console.log(this)
+            // set variable for pokeType
+        });
+        buildPlot("dragon");
+        // replace "dragon" with variable for Type
+        // plotInfo(data.names[0]);
+    });
+}
+
+init();
